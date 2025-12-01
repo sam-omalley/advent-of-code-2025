4960e3c9dc477f72e7b3ecf968eafd41dfa36efb
Samuel O'Malley <git@sam.omalley.cc>
2025-12-01 22:24:24 +1030

    Change result output to be more specific for AoC

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -14,7 +14,8 @@ fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
     let (password, num_spins) = get_password(&contents);
-    println!("The password is: {password}. Num spins: {num_spins}");
+    println!("The day-1-part-1 result is: {password}");
+    println!("The day-1-part-2 result is {num_spins}");
 
     Ok(())
 }
