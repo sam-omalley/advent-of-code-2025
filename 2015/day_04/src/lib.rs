93c78c0c2fd116af3c52940dbb66daceb42fcf96
Samuel O'Malley <git@sam.omalley.cc>
2025-12-13 22:44:46 +1030

    Complete day 04 2015 part 2

--- 2015/day_04/src/lib.rs
+++ 2015/day_04/src/lib.rs
@@ -1,8 +1,8 @@
-pub fn part_1(input: &str) -> usize {
+pub fn find_zero_prefix_md5_hash(input: &str, check: fn(&md5::Digest) -> bool) -> usize {
     let mut counter = 0;
     loop {
         let digest = md5::compute((input.to_string() + &counter.to_string()).as_bytes());
-        if check_digest(&digest) {
+        if check(&digest) {
             eprintln!("{input}{counter}: {:x}", digest);
             break;
         }
@@ -11,8 +11,20 @@ pub fn part_1(input: &str) -> usize {
     counter
 }
 
-pub fn check_digest(digest: &md5::Digest) -> bool {
+pub fn part_1(input: &str) -> usize {
+    find_zero_prefix_md5_hash(input, check_digest_part_1)
+}
+
+pub fn part_2(input: &str) -> usize {
+    find_zero_prefix_md5_hash(input, check_digest_part_2)
+}
+
+pub fn check_digest_part_1(digest: &md5::Digest) -> bool {
     digest[0] | digest[1] | (digest[2] >> 4) == 0
+}
+
+pub fn check_digest_part_2(digest: &md5::Digest) -> bool {
+    digest[0] | digest[1] | digest[2] == 0
 }
 
 #[cfg(test)]
